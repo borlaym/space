6a1139ce571e41cec9f195096c77dcfb3edeb215
Marton Borlay <borlaym@gmail.com>
2018-07-04 22:03:53 +0200

    nicer light

--- src/index.tsx
+++ src/index.tsx
@@ -11,12 +11,12 @@ texture.wrapS = THREE.RepeatWrapping;
 texture.wrapT = THREE.RepeatWrapping;
 texture.repeat.set(4, 4);
 
-const geometry = new THREE.CubeGeometry(4, 4, 4);
+const geometry = new THREE.CubeGeometry(4, 4, 4, 50, 50, 50);
 const material = new THREE.MeshLambertMaterial({ color: 0xaaaaaa, map: texture, side: THREE.DoubleSide });
 const cube = new THREE.Mesh(geometry, material);
 
-const light = new THREE.PointLight(0xffffff, 2, 100, 1);
-light.position.set(0, 0, 3);
+const light = new THREE.PointLight(0xffffff, 13, 4, 2);
+light.position.set(0, 0, 4);
 scene.add(light);
 
 scene.add(cube);

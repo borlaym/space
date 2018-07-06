8a6d1624b7bb73840354fbf0cfa380bf02799ed1
Marton Borlay <borlaym@gmail.com>
2018-07-06 13:37:35 +0200

    turning on y axis

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ texture.wrapS = THREE.RepeatWrapping;
 texture.wrapT = THREE.RepeatWrapping;
 texture.repeat.set(4, 4);
 
-const geometry = new THREE.CubeGeometry(4, 4, 4, 50, 50, 50);
+const geometry = new THREE.CubeGeometry(4, 4, 6, 50, 50, 50);
 const material = new THREE.MeshLambertMaterial({ color: 0xaaaaaa, map: texture, side: THREE.DoubleSide });
 const cube = new THREE.Mesh(geometry, material);
 
@@ -25,13 +25,24 @@ cube.position.setZ(4)
 camera.position.z = 5;
 
 const SPEED = 0.1;
+const TURN_SPEED = 4;
 
 interface InterfaceState {
-	keysDown: string[]
+	keysDown: string[],
+	lastMousePos: {
+		x?: number,
+		y?: number
+	}
+	mousePos: {
+		x?: number,
+		y?: number
+	}
 };
 
 const state: InterfaceState = {
-	keysDown: []
+	keysDown: [],
+	lastMousePos: {},
+	mousePos: {}
 };
 
 document.addEventListener('keydown', (event) => {
@@ -41,6 +52,12 @@ document.addEventListener('keydown', (event) => {
 document.addEventListener('keyup', (event) => {
 	state.keysDown = state.keysDown.filter(key => key !== event.key);
 });
+
+document.addEventListener('mousemove', (event) => {
+	state.mousePos.x = event.x;
+	state.mousePos.y = event.y;
+
+})
 
 function animate() {
 	if (state.keysDown.indexOf('w') > -1) {
@@ -56,7 +73,18 @@ function animate() {
 		camera.position.x += SPEED;
 	}
 
-
+	if (state.mousePos.x && state.mousePos.y) {
+		if (!state.lastMousePos.x || !state.lastMousePos.y) {
+			state.lastMousePos.x = state.mousePos.x;
+			state.lastMousePos.y = state.mousePos.y;
+		}
+		const dx = state.mousePos.x - state.lastMousePos.x;
+		camera.rotation.y -= dx / window.innerWidth * TURN_SPEED;
+		state.lastMousePos = {
+			x: state.mousePos.x,
+			y: state.mousePos.y
+		}
+	}
 
 	requestAnimationFrame(animate);
 	renderer.render(scene, camera);
